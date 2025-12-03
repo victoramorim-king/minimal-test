c421c5c5644ed67780a9100671e043b0418c4bf7
Victor Amorim <victoramorimchagas@gmail.com>
2025-12-03 04:06:09 -0300

    Update greeting message in index.js to include developer's name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello from Coolify minimal test!!');
+  res.send('Hello from Coolify minimal test!! Victor Amorim');
 });
 
 app.listen(PORT, HOST, () => console.log(`Server running on ${HOST}:${PORT}`));
